82184a357f1012efb9572080527281ca91da157e
wun-yu-lin <t928394558@gmail.com>
2022-07-25 23:27:57 +0800

    butten event handling in React

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,23 +1,40 @@
 import React from "react";
-import "./styles/css/style.css"; //import css
-import Friend from "./Friend";
 
-//Props (properties)
-//Every component in react has its own properties
+//event handling 1
+const buttonHandler1 = () => {
+  let date = new Date().toLocaleDateString();
+  alert(date);
+};
+
+//event handling 2
+const buttonHandler2 = (msg) => {
+  alert(msg);
+};
+
+//app.js
+// .addEventLister()
 
 const App = () => {
-  let friends = [
-    { name: "wunyu", age: 25, desc: "Nice" },
-    { name: "emma", age: 25, desc: "Nice" },
-    { name: "winnie", age: 25, desc: "Nice" },
-  ];
   return (
     <div>
-      <h1>{Math.random()}</h1>
-      <h1>My friend:</h1>
-      {friends.map((friend) => (
-        <Friend name={friend.name} age={friend.age} disc={friend.desc} />
-      ))}
+      <form action="">
+        <input type="text" />
+        {/* button 加上 onClick後面加上 event handling 的 function */}
+        <button onClick={buttonHandler1}>Submit</button>
+      </form>
+
+      <br />
+      {/* 透過arrow function避免JS中，直接代入parameter時，function會直接執行的情況 */}
+      <form action="">
+        <input type="text" />
+        <button
+          onClick={() => {
+            buttonHandler2("Hello");
+          }}
+        >
+          Alart hello
+        </button>
+      </form>
     </div>
   );
 };
--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import Nav from "./Nav";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Nav />
     <App />
   </React.StrictMode>,
   document.querySelector("#root")
